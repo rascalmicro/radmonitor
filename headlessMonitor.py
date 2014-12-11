79e15c0997be400d64c8b7e875d7a8a88af79965
Brandon Stafford <brandon@pingswept.org>
2014-12-11 14:26:13 -0500

    Some changes from 2014-12-02 image provided by Tyler Cox

--- headlessMonitor.py
+++ headlessMonitor.py
@@ -12,103 +12,140 @@ lock is lost, the data is simply dropped.
 """
 
 import emorpho, time, psycopg2
-import gps
-from datetime import datetime
-import dateutil.parser
-from statusLED import StatusLED
-
-led = StatusLED(18)
+from numpy import isnan, mean, sqrt, square
+from util.wifi import WiFi as wifi
+from util.gpsd import GPSD as gps
+import os, signal, sys, json, datetime
+from geopy import distance
 
 SAMPLE_TIME = 2 # seconds
-session = gps.gps(mode=gps.WATCH_ENABLE)
+wifi = wifi()
+TZ = ""
+sensor = "0"
+version = "0.0"
+gpsBase = (0.0, 0.0)
+gpsRange = 100
 
-try:
-    # try:                        # try to connect to remote db if it's available
-    #     conn = psycopg2.connect(database = 'radiation', user = 'radiation',
-    #                 password = 'radiation', host = '10.42.0.1')
-    #     print("Using remote database")
-    # except:
-    conn = psycopg2.connect(database = 'radiation', user = 'radiation',
-                password = 'radiation', host = 'localhost')
-    print("Using local database")
-    cur = conn.cursor()
-    led.setStatus('on')
-except:
-    print('No Postgres connection')
-    led.setStatus('off')
-    raise SystemExit
+def loadConfig():
+  json_config = open('/home/debian/radmonitor-master/radmonitor.config')
+  config = json.load(json_config)
+  json_config.close()
+  for setting in config["radsettings"]:
+    setattr(e, setting, config["radsettings"][setting])
+  SAMPLE_TIME = config["sampleTime"]
+  e.clearStats()
+  e.startTimedHistogram(SAMPLE_TIME)
+  return config
 
+def haltMonitor(signal, frame):
+  wifi.stop()
+  gps.stop()
+  print "Exiting headlessMonitor cleanly"
+  sys.exit(0)
+
+signal.signal(signal.SIGINT, haltMonitor)
+
+def connectToRad():
+  e.scan()
+  while e.open(0) == False:
+    print "Could not connect to Rad Sensor"
+    os.system("echo heartbeat > /sys/class/leds/beaglebone\:green\:usr2/trigger")
+    time.sleep(5)
+    e.scan()
+  os.system("echo none > /sys/class/leds/beaglebone\:green\:usr2/trigger")
+
+conn = psycopg2.connect(database = 'radiation', user = 'radiation',
+                        password = 'radiation', host = 'localhost')
+
+cur = conn.cursor()
+
+wifi.start()
 
 e = emorpho.eMorpho()
-e.scan()
-e.open(0)
+connectToRad()
 
-# Set up with values provided by Bridgeport. This should stay in sync with
-# monitor.py
-e.HV = 1025
-e.fineGain = 43500
-e.gain = 10100
-e.compression = 7
-e.holdOff = 1500
-e.pulseThreshold = 10
-e.pileUp = 0
-e.clearStats()
-e.startTimedHistogram(SAMPLE_TIME)
+config = loadConfig()
 
-def getFix(session):
-    """Use gpsd session to get a TPV report. Return None if no fix is currently
-       available. Return gpsd's dict otherwise."""
-    report = session.next()
-    if report['class'] == 'DEVICE':
-        # Clean up our current connection.
-        session.close()
-        # Tell gpsd we're ready to receive messages.
-        session = gps.gps(mode=gps.WATCH_ENABLE)
-    elif report['class'] == 'TPV':
-        if report['mode'] == 3:
-            return report
-        else:
-            return None
-        
+gpsBase = (config["baseCoords"]["latitude"], config["baseCoords"]["longitude"])
+gpsRange = config["baseCoords"]["range"]
+sensor = config["sensor"]
+version = config["version"]
+TZ = config["timezone"]
+AdjustTime = config["gpsTime"]
+
+gps = gps(TZ, AdjustTime)
+gps.start()
+
+# def getFix(session):
+#    """Use gpsd session to get a TPV report. Return None if no fix is currently
+#       available. Return gpsd's dict otherwise."""
+
+#    for report in session:
+#        if report["class"] == "TPV":
+#            if report["mode"] == 3:
+#                return report
+#            else:
+#                return None
+
+state = gpsState = True
+gpsTimeout = 0
+sessionCounter = 0
+
 while True:
     time.sleep(SAMPLE_TIME)
-    location = getFix(session)
-    if location is None:
-        led.setStatus('error')
-        # print("Error: No GPS fix")
+    sessionCounter = sessionCounter + 1
+    if sessionCounter > 1000:
+      loadConfig()
+      sessionCounter = 0
+#    location = gps.getFix()
+    location = gps.getLocation()
+    if location.longitude == 0.0 or isnan(location.longitude):
+        print "Error: No GPS location"
+        gpsTimeout = gpsTimeout + 1
+        if gpsState:
+          os.system("echo default-on > /sys/class/leds/beaglebone\:green\:usr2/trigger")
+          gpsState = False
+        else:
+          os.system("echo none > /sys/class/leds/beaglebone\:green\:usr2/trigger")
+          gpsState = True 
+        if gpsTimeout >= 5:
+          print "Restarting GPS"
+          gpsTimeout = 0
+          gps.retry()
         continue
     else:
-        # print(location['time'])
-        led.setStatus('on')
+       baseDistance = distance.vincenty(gpsBase, (location.latitude, location.longitude)).meters 
+       if baseDistance <= gpsRange:
+         if wifi.running == False:
+           print "In range of router"
+           wifi.start()
 
     hist = e.readHistogram()
     if hist == False:
-        led.setStatus('error')
         print "Error: Couldn't read the histogram"
+        connectToRad()
         continue
-    else:
-        led.setStatus('on')
 
     stats = e.readStats()
     e.startTimedHistogram(SAMPLE_TIME)
 
     # TODO: There's some logic in monitor.py to handle GPS dropouts by
-    # recording HDOP of -1. We should replicate that here.
-    if location:
-        cur.execute("INSERT INTO histograms (location, hdop, time, sampletime, temp, cps, histogram, altitude) " +
-                "VALUES (ST_GeomFromText('POINT(%s %s)', 4326), %s, %s, %s, %s, %s, %s, %s)",
-                (location["lon"], location["lat"], location["mode"], dateutil.parser.parse(location.time), SAMPLE_TIME,
-                 e.getTemperature(), stats["cps"], hist, location["alt"]))
-    else:
-        cur.execute("INSERT INTO histograms (location, hdop, time, sampletime, temp, cps, histogram, altitude) " +
-                    "VALUES (ST_GeomFromText('POINT(0 0)', 4326), %s, %s, %s, %s, %s, %s, %s)",
-                (location["lon"], location["lat"], -1, dateutil.parser.parse(location.time), SAMPLE_TIME,
-                 e.getTemperature(), stats["cps"], hist, location["alt"]))
-    # cur.execute("INSERT INTO histograms (location, hdop, time, sampletime, temp, cps, histogram, altitude) " +
-    #             "VALUES (ST_GeomFromText('POINT(%s %s)', 4326), %s, %s, %s, %s, %s, %s, %s)",
-    #             (location["lon"], location["lat"], -1, datetime.fromtimestamp(location.time), SAMPLE_TIME,
-    #              e.getTemperature(), stats["cps"], hist, location["alt"]))
+    # recording HDOP of -1. We should replicate that here.a
+    cur.execute("INSERT INTO histograms (location, hdop, time, sampletime, temp, cps, histogram, altitude, sensor, version) " +
+                "VALUES (ST_GeomFromText('POINT(%s %s)', 4326), %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                (location.longitude, location.latitude, location.gpsError, location.timestamp, SAMPLE_TIME,
+                 e.getTemperature(), 
+                 stats["cps"], 
+                 hist, 
+                 location.altitude, 
+                 sensor, 
+                 version))
     conn.commit()
 
-e.close()
-conn.close()
+    print "Added Entry ", location.timestamp, stats["cps"], location.latitude, location.longitude, location.gpsError, distance.vincenty(gpsBase, (location.latitude, location.longitude)).meters
+    if state:
+      os.system("echo default-on > /sys/class/leds/beaglebone\:green\:usr1/trigger")
+      state = False
+    else:
+      os.system("echo none > /sys/class/leds/beaglebone\:green\:usr1/trigger")
+      state = True
